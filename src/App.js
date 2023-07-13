b6f9d4fa4125cf62940498b1b73a02a01979cd4f
John <jddhalogamer@gmail.com>
2023-07-13 15:43:06 -0500

    added dark mode functionality, and built menu sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom'
+import { useState } from 'react'
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from './utils/Theme'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Menu from './components/Menu'
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`;
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
 
 function App() {
+
+const [ darkMode, setDarkMode ] = useState(true)
+
   return (
+  <ThemeProvider theme={ darkMode ? darkTheme : lightTheme }>
   <BrowserRouter>
-    <Navbar />
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+          <Wrapper>
+            <Routes>
+              <Route path="/">
+                <Route index element={'/'} />
+                  <Route path="signin" element={'/'} />
+                  <Route path="video">
+                  <Route path=":id" element={'/'} />
+                  </Route>
+                </Route>
+              </Routes>
+          </Wrapper>
+      </Main>
+    </Container>
   </BrowserRouter>
+</ThemeProvider>
   );
 }
 
